8fa1767e939f047311fd79f1769bc4679b9befc9
Gerrit Grunwald <han.solo@mac.com>
2019-12-03 17:50:12 +0100

    Switched to Java13 and added jpackage scripts to build native app
    Added the scripts to build a native package. The scripts are taken from https://github.com/dlemmermann/JPackageScriptFX. Please check out what to do to build the native app there.

---
+++ build_app.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# ------ ENVIRONMENT --------------------------------------------------------
+# The script depends on various environment variables to exist in order to
+# run properly. The java version we want to use, the location of the java
+# binaries (java home), and the project version as defined inside the pom.xml
+# file, e.g. 1.0-SNAPSHOT.
+#
+# PROJECT_VERSION: version used in pom.xml, e.g. 1.0-SNAPSHOT
+# APP_VERSION: the application version, e.g. 1.0.0, shown in "about" dialog
+
+JAVA_VERSION=13
+MAIN_JAR="SpaceFX-1.0-SNAPSHOT.jar"
+
+echo "java home: $JAVA_HOME"
+echo "jpackage home: $JPACKAGE_HOME"
+echo "project version: $PROJECT_VERSION"
+echo "app version: $APP_VERSION"
+echo "main JAR file: $MAIN_JAR"
+
+# ------ SETUP DIRECTORIES AND FILES ----------------------------------------
+# Remove previously generated java runtime and installers. Copy all required
+# jar files into the input/libs folder.
+
+rm -rfd ./build/java-runtime/
+rm -rfd build/installer/
+
+mkdir -p build/installer/input/libs/
+
+cp build/libs/* build/installer/input/libs/
+cp build/libs/${MAIN_JAR} build/installer/input/libs/
+
+# ------ REQUIRED MODULES ---------------------------------------------------
+# Use jlink to detect all modules that are required to run the application.
+# Starting point for the jdep analysis is the set of jars being used by the
+# application.
+
+echo "detecting required modules"
+detected_modules=`$JAVA_HOME/bin/jdeps \
+  --multi-release ${JAVA_VERSION} \
+  --ignore-missing-deps \
+  --print-module-deps \
+  --class-path "build/installer/input/libs/*" \
+    build/classes/java/main/eu/hansolo/spacefx/SpaceFX.class`
+echo "detected modules: ${detected_modules}"
+
+
+# ------ MANUAL MODULES -----------------------------------------------------
+# jdk.crypto.ec has to be added manually bound via --bind-services or
+# otherwise HTTPS does not work.
+#
+# See: https://bugs.openjdk.java.net/browse/JDK-8221674
+#
+# In addition we need jdk.localedata if the application is localized.
+# This can be reduced to the actually needed locales via a jlink paramter,
+# e.g., --include-locales=en,de.
+
+manual_modules=jdk.crypto.ec,jdk.localedata
+echo "manual modules: ${manual_modules}"
+
+# ------ RUNTIME IMAGE ------------------------------------------------------
+# Use the jlink tool to create a runtime image for our application. We are
+# doing this is a separate step instead of letting jlink do the work as part
+# of the jpackage tool. This approach allows for finer configuration and also
+# works with dependencies that are not fully modularized, yet.
+
+echo "creating java runtime image"
+$JAVA_HOME/bin/jlink \
+  --no-header-files \
+  --no-man-pages  \
+  --compress=2  \
+  --strip-debug \
+  --add-modules "${detected_modules},${manual_modules}" \
+  --include-locales=en,de \
+  --output build/java-runtime
+
+# ------ PACKAGING ----------------------------------------------------------
+# A loop iterates over the various packaging types supported by jpackage. In
+# the end we will find all packages inside the build/installer directory.
+
+for type in "app-image" "dmg" "pkg"
+do
+  echo "Creating installer of type ... $type"
+
+  $JPACKAGE_HOME/bin/jpackage \
+  --type $type \
+  --dest build/installer \
+  --input build/installer/input/libs \
+  --name SpaceFX \
+  --main-class eu.hansolo.spacefx.Launcher \
+  --main-jar ${MAIN_JAR} \
+  --java-options -Xmx2048m \
+  --runtime-image build/java-runtime \
+  --icon src/main/resources/eu/hansolo/spacefx/icon.icns \
+  --app-version ${APP_VERSION} \
+  #--vendor "Alliance" \
+  #--copyright "Copyright © 2019 Alliance Inc." \
+  #--mac-package-identifier eu.hansolo.spacefx \
+  #--mac-package-name Alliance
+done
