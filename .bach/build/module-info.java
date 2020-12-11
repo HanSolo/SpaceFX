fa052da60d7c464c0f2f4a69c0f66da2e6bb072a
Christian Stein <sormuras@gmail.com>
2020-12-11 12:37:13 +0100

    Clean up project-info

--- .bach/build/module-info.java
+++ .bach/build/module-info.java
@@ -1,19 +1,10 @@
 import com.github.sormuras.bach.project.Feature;
 import com.github.sormuras.bach.project.ProjectInfo;
-import com.github.sormuras.bach.project.ProjectInfo.ExternalModules;
-import com.github.sormuras.bach.project.ProjectInfo.ExternalModules.Link;
 
 @ProjectInfo(
     version = "1.0",
     features = Feature.GENERATE_CUSTOM_RUNTIME_IMAGE,
-    launchCustomRuntimeImageWithModule = "spacefx/eu.hansolo.spacefx.Launcher",
-    externalModules =
-        @ExternalModules(
-            links = {
-              @Link(module = "jpro.webapi", to = "https://sandec.bintray.com/repo/com/sandec/jpro/jpro-webapi/2020.1.1/jpro-webapi-2020.1.1.jar"),
-              @Link(module = "one.jpro.sound", to = "https://sandec.bintray.com/repo/one/jpro/one.jpro.sound/0.1.0/one.jpro.sound-0.1.0.jar")
-            }
-        )
+    launchCustomRuntimeImageWithModule = "spacefx/eu.hansolo.spacefx.Launcher"
 )
 module build {
   requires com.github.sormuras.bach;
